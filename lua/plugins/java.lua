5430c2c36999b98686153c897ae740a21e32b470
villainru <electro.dmitriy@gmail.com>
2026-02-04 20:06:36 +0400

    Fix java path for windows compatibility

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -2,16 +2,23 @@ return {
   {
     "mfussenegger/nvim-jdtls",
     opts = function(_, opts)
-      -- Укажите полный путь к собранному вами файлу
-      local bundle = vim.fn.glob(
-        "/home/villainru/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar", true)
+      -- 1. Добавляем проверку системы
+      local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
 
-      -- Добавляем этот файл в настройки JDTLS
-      opts.jdtls = opts.jdtls or {}
-      opts.jdtls.init_options = opts.jdtls.init_options or {}
-      opts.jdtls.init_options.bundles = { bundle }
+      -- 2. Заворачиваем Linux-специфичный код в условие
+      if not is_windows then
+        local bundle = vim.fn.glob(
+          "/home/villainru/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar", true)
 
-      -- Включаем интеграцию с DAP
+        -- Проверяем, нашлся ли файл, чтобы не передавать пустую строку
+        if bundle ~= "" then
+          opts.jdtls = opts.jdtls or {}
+          opts.jdtls.init_options = opts.jdtls.init_options or {}
+          opts.jdtls.init_options.bundles = { bundle }
+        end
+      end
+
+      -- Включаем интеграцию с DAP (это безопасно)
       opts.dap = { hotcodereplace = 'auto' }
     end,
   },
